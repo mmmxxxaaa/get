97e09326ff0728c985cc785e5bc39333362b0aa5
Максим <katkov.ma@phystech.edu>
2026-04-15 19:31:34 +0300

    Предварительная версия 8-битного sin(t)

---
+++   get-dac/r2r-sin.py
@@ -0,0 +1,16 @@
+import r2r_dac as r2r
+import signal_generator as sg
+import time
+
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+
+try:
+    dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], amplitude, True)
+    while True:
+        dac.set_voltage(amplitude * sg.get_sin_wave_amplitude(signal_frequency, time.monotonic()))
+        sg.wait_for_sampling_period(sampling_frequency)
+    
+finally:
+    dac.deinit()
