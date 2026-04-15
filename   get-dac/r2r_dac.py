52c6ae0922e759c1995ff5bf86eb73ffbe49f0d8
Максим <katkov.ma@phystech.edu>
2026-04-15 18:27:55 +0300

    8-битный R2R ЦАП в виде класса

---
+++   get-dac/r2r_dac.py
@@ -0,0 +1,52 @@
+import RPi.GPIO as GPIO
+
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose=False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
+
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    
+    def set_number(self, number):
+        if number < 0:
+            number = 0
+        if number > 255:
+            number = 255
+
+        bits = [int(x) for x in bin(number)[2:].zfill(8)]
+        GPIO.output(self.gpio_bits, bits)
+
+        if self.verbose:
+            print(f"Число на вход ЦАП: {number}, биты: {bits}")
+    
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(
+                f"Напряжение выходит за динамический диапазон ЦАП "
+                f"(0.00 - {self.dynamic_range:.2f} B)"
+            )
+            return
+        
+        number = int(voltage / self.dynamic_range * 255)
+        self.set_number(number)
+
+if __name__ == "__main__":
+    dac = None
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.3, True)
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах: "))
+                dac.set_voltage(voltage)
+                print()
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+    finally:
+        if dac is not None:
+            dac.deinit()
